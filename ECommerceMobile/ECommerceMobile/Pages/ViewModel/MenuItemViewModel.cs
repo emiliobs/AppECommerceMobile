969e9a632ba28a8fd03779b3f084ed83063787a2
pc <barrera_emilio@yahoo.es>
2016-12-06 00:30:26 +0100

    service,navigateservice class

--- ECommerceMobile/ECommerceMobile/Pages/ViewModel/MenuItemViewModel.cs
+++ ECommerceMobile/ECommerceMobile/Pages/ViewModel/MenuItemViewModel.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 
 namespace ECommerceMobile.Pages.ViewModel
 {
     public class MenuItemViewModel
     {
+        #region Properties
         public string Icon { get; set; }
 
         public string Title { get; set; }
 
         public string PageName { get; set; }
+
+
+        #endregion
+
+        #region Commmands
+        public ICommand NavigateCommand
+        {
+            get
+            {
+                return new RelayCommand(Navigate);
+            }
+        }
+
+
+            private void Navigate()
+            {
+                throw new NotImplementedException();
+            }
+
+
+        #endregion
+
+
     }
 }
---
+++ ECommerceMobile/ECommerceMobile/Services/NavigationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMobile.Services
+{
+     public class NavigationService
+    {
+
+         public async Task Navigate(string pageName)
+         {
+             switch (pageName)
+             {
+
+             }
+         }
+     }
+}
